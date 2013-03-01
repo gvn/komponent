555d02e206c7e1108b5ea536d462a6a2029fae9b
Gvn Suntop <gavin@gsuntop.com>
2013-03-01 15:09:45 -0800

    fixing bash script

--- jsc.sh
+++ jsc.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-echo -n "Component Name: "
-read componentName
-componentName=${componentName:-Component}
+function jsc() {
+    echo -n "Component Name: "
+    read componentName
+    componentName=${componentName:-Component}
 
-echo -n "Namespace (window): "
-read namespace
-namespace=${namespace:-window}
+    echo -n "Namespace (window): "
+    read namespace
+    namespace=${namespace:-window}
 
-echo -n "Author: "
-read author
+    echo -n "Author: "
+    read author
 
-# strip spaces from class name
-local componentNameNoSpace="$(echo $componentName | tr -d ' ')"
+    # strip spaces from class name
+    local componentNameNoSpace="$(echo $componentName | tr -d ' ')"
 
-# convert filename to all lowercase and prepend namespace if specified
-if [ "$namespace" != "window" ]; then
-    local filename="$(echo $namespace.$componentNameNoSpace.js | tr '[:upper:]' '[:lower:]')"
-else
-    local filename="$(echo $componentNameNoSpace.js | tr '[:upper:]' '[:lower:]')"
-fi
+    # convert filename to all lowercase and prepend namespace if specified
+    if [ "$namespace" != "window" ]; then
+        local filename="$(echo $namespace.$componentNameNoSpace.js | tr '[:upper:]' '[:lower:]')"
+    else
+        local filename="$(echo $componentNameNoSpace.js | tr '[:upper:]' '[:lower:]')"
+    fi
 
-curl https://raw.github.com/gvn/component-pattern/master/component.js > $filename
+    curl https://raw.github.com/gvn/component-pattern/master/component.js > $filename
 
-# convert tokens within scaffold to specified values
-sed -i .bak "s/Component/$componentNameNoSpace/g" $filename
-sed -i .bak "s/window/$namespace/g" $filename
-sed -i .bak "s/#AUTHOR#/$author/g" $filename
+    # convert tokens within scaffold to specified values
+    sed -i .bak "s/Component/$componentNameNoSpace/g" $filename
+    sed -i .bak "s/window/$namespace/g" $filename
+    sed -i .bak "s/#AUTHOR#/$author/g" $filename
 
-rm $filename.bak
+    rm $filename.bak
+}
+
+jsc
